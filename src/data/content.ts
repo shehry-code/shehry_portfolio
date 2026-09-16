c6485254d658969b72b3b75cca991cc1ea0633f6
Shehry-code <shehrimary@gmail.com>
2026-09-16 17:00:16 +0500

    new blog is added

--- src/data/content.ts
+++ src/data/content.ts
@@ -285,7 +285,21 @@ export const blogPosts: BlogPost[] = [
     readingTime: 5,
     content: blogContent["../content/blogs/why-learn-computer-architecture.md"],
   },
+  {
+    slug: "How user define functions work at assembly level",
+    title: "How User-Defined Functions Work at the Assembly Level",
+    description: "Exploring the mechanics of function calls and returns in assembly language.And how stack is used to manage function calls.",
+    date: "2026-10-01",
+    tags: ["Systems", "Assembly", "Nasm", "CPU"],
+    category: "Systems",
+    featured: true,
+    draft: false,
+    readingTime: 10,
+    content: blogContent["../content/blogs/what-happens-when-program-runs.md"],
+  },
+
 ];
+
 
 // ============================================================
 // NOTES
